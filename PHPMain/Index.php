bda90957454f194cf94e43d88f434d383a711efa
emacslisp <wudi850620@hotmail.com>
2017-02-09 11:23:24 +1100

    add php functions

--- PHPMain/Index.php
+++ PHPMain/Index.php
@@ -4,4 +4,16 @@ $x = 1;
 
 echo ++$x;
 
+//@example: phpversion api
+echo 'Current PHP version: ' . phpversion(). "<br>";
+
+//@example: php ini_get
+echo 'display_errors = ' . ini_get('display_errors') . "<br>";
+echo 'register_globals = ' . ini_get('register_globals') . "<br>";
+echo 'post_max_size = ' . ini_get('post_max_size') . "<br>";
+echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "<br>";
+echo 'post_max_size in bytes = ' . ini_get('post_max_size') ."<br>";
+
+
+
 ?>
