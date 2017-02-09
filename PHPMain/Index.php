28e067e32511608d0cfaacbd95b12bbfab10351c
emacslisp <wudi850620@hotmail.com>
2017-02-09 11:37:56 +1100

    add more php functions

--- PHPMain/Index.php
+++ PHPMain/Index.php
@@ -15,5 +15,20 @@ echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "<br>";
 echo 'post_max_size in bytes = ' . ini_get('post_max_size') ."<br>";
 
 
+//@example: php version_compare
+if (version_compare(phpversion(), '5.3.10', '<')) {
+	echo("php version isn't high enough");
+}
+else 
+	echo 'php version is high enough';
 
+	echo '<br>';
+	
+	//@example: php init_get and init_set
+	@ini_set( 'magic_quotes_runtime', 0 );
+	$xx = ini_get( 'magic_quotes_runtime');
+	echo $xx;
+	
+	echo ini_get( 'magic_quotes_runtime');
+	@ini_set( 'magic_quotes_runtime', $xx );
 ?>
