27c0bea463232d8dfbcc5ed5eafe689cbfbc453e
emacslisp <wudi850620@hotmail.com>
2017-02-23 16:59:58 +1100

    example of using add_filters wordpress api and so on

--- wordpress2/test.php
+++ wordpress2/test.php
@@ -13,6 +13,10 @@ function test_func( $text, $var1, $var2 ){
 	return $text . $var1 . $var2;
 }
 
+function test_func2( $text, $var1, $var2 ){
+	return $text . $var1 . $var2;
+}
+
 
  echo "###### " . $wpdb->db_version();
   
@@ -22,11 +26,18 @@ function test_func( $text, $var1, $var2 ){
  echo $wpdb->get_var('select * from wp_users');
  $wpdb->close();
 
-
+//@example: php - wordpress - add_filter, apply_filters, add_action, do_action example
 add_filter("test", "test_func", 10, 3);
 
 $value =  apply_filters("test", 'arg1','arg2','arg3'); 
  echo $value;
  
- echo '<br> it is working';
+ echo '<br> it is working xxx ';
+ 
+ add_action("test_action","test_func2",10,3);
+ echo '<br> do_action test: ';
+ echo do_action("test_action",'arg1','arg2','arg3')
+ 
+ 
+ 
 ?>    
