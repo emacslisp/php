819bf8af004a3d65185ad7d43c8bb452205a8267
emacslisp <wudi850620@hotmail.com>
2017-04-05 15:00:16 +1000

    wordpress core - simulate add_doaction and apply_doaction

---
+++ PHPMain/arrayTest.php
@@ -0,0 +1,21 @@
+<?php
+
+// @example: wordpress core - simulate add_doaction and apply_doaction
+function test() {
+	echo 'hello world';
+}
+
+$priority = 0;
+$function_to_add='test';
+$idx = $function_to_add;
+
+$callbacks = array();
+
+$callbacks[$priority][$idx] = array(
+			'function' => $function_to_add,
+			'accepted_args' => $accepted_args
+		);
+
+call_user_func_array($callbacks[$priority][$idx]['function'],array());
+
+?>
