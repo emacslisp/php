8190106b830c58f556432b7ac5c1e89e3f3716d7
emacslisp <java.gdb@gmail.com>
2017-06-27 12:02:01 +1000

    add debugging info

--- PHPMain/Date.php
+++ PHPMain/Date.php
@@ -1,12 +1,12 @@
 <?php
-debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-file_put_contents('/Users/ewu/output.log',print_r((new Exception)->getTraceAsString(),true). PHP_EOL . PHP_EOL,FILE_APPEND);
 
 function d() {
-	var_dump((new Exception)->getTraceAsString());
-	debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-	error_log(var_export((new Exception)->getTraceAsString(), true));
-	file_put_contents('/Users/ewu/output.log',print_r((new Exception)->getTraceAsString(),true). PHP_EOL . PHP_EOL,FILE_APPEND);
+	var_dump(__FILE__.':('.__LINE__.') '.__FUNCTION__.PHP_EOL.(new Exception)->getTraceAsString());
+	debug_print_backtrace();
+	echo PHP_EOL;
+	var_dump(debug_backtrace());
+	error_log(var_dump((new Exception)->getTraceAsString(), true));
+	file_put_contents('/Users/ewu/output.log',print_r(__FILE__.':('.__LINE__.') '.__FUNCTION__.PHP_EOL.(new Exception)->getTraceAsString(),true). PHP_EOL . PHP_EOL,FILE_APPEND);
 }
 
 function c() {
