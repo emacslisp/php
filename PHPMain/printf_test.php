9b06080eaeea12fde1253b634b642ea85d8862ae
emacslisp <java.gdb@gmail.com>
2017-07-20 10:56:28 +1000

    add printf test

--- PHPMain/printf_test.php
+++ PHPMain/printf_test.php
@@ -11,5 +11,14 @@ First %d converts a float to an int by truncation.
 Second floats are notorious for tiny little rounding errors.
  * */
 
+function __($text) {
+	return $text;
+}
+
 printf ("(9.95 * 100) = %d \n", (9.95 * 100)); 
+
+printf(__("test %s"), '<b> printf Test</b>');
+
+$prefix = 'wordpress';
+echo "Select $prefix";
 ?>
