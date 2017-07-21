ccc9de30734544d5c28227c2b192028d17bb2806
emacslisp <java.gdb@gmail.com>
2017-07-21 11:04:30 +1000

    add more html entities example

--- PHPMain/html_entities.php
+++ PHPMain/html_entities.php
@@ -8,4 +8,14 @@ echo htmlentities($str);
 // Outputs: A &#039;quote&#039; is &lt;b&gt;bold&lt;/b&gt;
 echo htmlentities($str, ENT_QUOTES);
 
+
+$str = "\x8F!!!";
+echo "==================<br>";
+// Outputs an empty string
+echo htmlentities($str, ENT_QUOTES, "UTF-8");
+
+echo "==================<br>";
+// Outputs "!!!"
+echo htmlentities($str, ENT_QUOTES | ENT_IGNORE, "UTF-8");
+
 ?>
